d625a5afa7ad0860af2240faa6b061dbd48f97d5
BenP123777s107063 <149143032+BenP123777s107063@users.noreply.github.com>
2023-10-27 07:03:47 +0000

    ok

--- speedtest.py
+++ speedtest.py
@@ -8,9 +8,9 @@ DONE = True
 
 try:
     for _ in tqdm.tqdm(range(5000)):
-        if done:
+        if DONE is True:
             state = env.reset()
-            done = False
+            DONE = False
         else:
             state, reward, done, info = env.step(env.action_space.sample())
 except KeyboardInterrupt:
